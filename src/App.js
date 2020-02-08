b10b74bb1dc1f0d5717188c9f740db2a4c818ef3
nikxtaco <nikitamenon2510@gmail.com>
2020-02-08 12:24:19 +0530

    header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header.js'
+
+function App() {
+  return (
+    <div className="Main">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
