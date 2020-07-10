f20410dd062360eb1424726d0d3904ab0923dc4d
unknown <nikitamenon2510@gmail.com>
2020-07-10 09:56:48 +0530

    blog_landing update

--- src/pages/Blog/Featured/featured.js
+++ src/pages/Blog/Featured/featured.js
@@ -18,27 +18,25 @@ const SecondPage = () => {
         <HLink to={"/blog/" + String(data.section)}>
          {/* <img src={require("../../img/blog/" + String(data.imageUrl) + ".jpg")} className="featured_image"></img> */}
 
-        <h3 className="filler_text_blog">
+        <h1 className="title">
         {data.title}
-        </h3>
+        </h1>
+
+        <hr className = "hr_title"/>
 
         <h5 className="blog_text_small">
-        {data.desc}
+        {data.year}
         </h5>
       </HLink>
 
-      <h5 className="filler_text_unlinked">
+      <h6 className="filler_text_unlinked">
         {data.moreText}
-      </h5>
+      </h6>
 
       <HLink to={"/blog/" + String(data.section)}>
-      <h5 className="blog_text_red">
-        {data.moreLinkText}
-        <UseAnimations
-        animationKey="skipForward"
-        size={20}
-        style={{ color: "white", cursor: "pointer", float:"right", padding:"0", margin:"0" }}
-      />
+      <h5 className="read_more">
+        {/* {data.moreLinkText} */}
+        Read More
       </h5>
       </HLink>
       </div>
@@ -51,8 +49,8 @@ return(
       <Navbar />
       
       <div className="main_container">
-      <h1 className="main_quote">
-      “ The very substance of the ambitious is merely the shadow of a dream. ”
+      <h1 className="main_heading">
+      Featured
       </h1>      
 
       {/* <h2 className="blog_text_big">
@@ -61,37 +59,6 @@ return(
 
 
      {posts}
-
-      <button className="home_button home_button_1 blog_button">
-        <h6 classname="home_button_text">
-          To Browse More
-        <UseAnimations
-        animationKey="arrowDown"
-        size={20}
-        style={{ color: "white", cursor: "pointer", float:"right", padding:"0", margin:"0", paddingLeft:"60px" }}
-      />
-        </h6>
-      </button>
-
-      <h5 className="blog_text_white">
-        Click here to check out my instagram (@cryptic.tales).
-        <a href="https://instagram.com/cryptic.tales">
-        <UseAnimations
-        animationKey="instagram"
-        size={20}
-        style={{ color: "white", cursor: "pointer", float:"right", padding:"0", margin:"0" }}
-      />
-      </a>
-      </h5>
-
-      <h5 className="blog_text_white">
-        I also write on Medium, at times.
-        <UseAnimations
-        animationKey="skipForward"
-        size={20}
-        style={{ color: "white", cursor: "pointer", float:"right", padding:"0", margin:"0" }}
-      />
-      </h5>
 
     </div>
     
