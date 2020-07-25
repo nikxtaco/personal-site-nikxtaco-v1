ed05bd4ed81d6f0cb3d06e7352c898cf9358d688
unknown <nikitamenon2510@gmail.com>
2020-07-25 16:03:29 +0530

    desktop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React from "react";
 class FullPage extends React.Component {
   render() {
     return (
-      <div style={{fontSize:"300px"}}>
+      <div style={{fontSize:"20rem"}}>
         heyya
         {/* <Transition1 />  */}
       </div>          
@@ -16,73 +16,3 @@ class FullPage extends React.Component {
 }
 
 export default FullPage;
-
-// import React from "react";
-// import { Pager } from "react-bootstrap";
-
-// import ReactPageScroller from "react-page-scroller";
-
-// import Transition1 from "./transitions/transition1/transition1.js"
-// import Transition2 from "./transitions/transition2/transition2.js"
-
-// import './App.css';
-
-// // import FirstComponent from "./pages/FirstComponent";
-// // import SecondComponent from "./pages/SecondComponent";
-// // import Sections from "./pages/Sections";
-// // import Vertical from "./pages/VerticalTimeline.js"
-// // import Footer from "./Footer.js"
-
-// class FullPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { currentPage: null };
-//   }
-
-
-//   handlePageChange = number => {
-//     this.setState({ currentPage: number }); // set currentPage number, to reset it from the previous selected.
-//   };
-
-//   getPagesNumbers = () => {
-//     const pageNumbers = [];
-
-//     for (let i = 1; i <= 9; i++) {
-//       pageNumbers.push(
-//         <Pager.Item key={i} eventKey={i - 1} onSelect={this.handlePageChange}>
-//           {i}
-//         </Pager.Item>,
-//       );
-//     }
-
-//     return [...pageNumbers];
-//   };
-
- 
-
-//   render() {
-//     const pagesNumbers = this.getPagesNumbers();
-
-//     return (
-//       <body>
-//       <React.Fragment>
-//         <ReactPageScroller
-//           pageOnChange={this.handlePageChange}
-//           customPageNumber={this.state.currentPage}
-//         >
-        
-//         <Transition1 />           
-//         <Transition2 />
-
-//         </ReactPageScroller>
-        
-//          {/* <Pager className="pagination-additional-class" bsSize="large">
-//            {pagesNumbers}
-//      </Pager> */}
-//       </React.Fragment>
-//       </body>
-//     );
-//   }
-// }
-
-// export default FullPage;
