93ca8bb665626794feadec8fba08c669ee450b47
unknown <nikitamenon2510@gmail.com>
2020-07-26 22:48:53 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Transition1 from "./pages/Home/home.js"
 import './App.css';
 
 import ReactGA from 'react-ga';
-ReactGA.initialize('UA-173520154-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-173520154-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 class FullPage extends React.Component {
   render() {
     return (
       <div>
+        {initializeReactGA()}
         <Transition1 /> 
       </div>          
     );
