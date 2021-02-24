8338b15538f02cd837ffd5a907bfaaf144bb346b
nikxtaco <nikitamenon2510@gmail.com>
2021-02-24 15:53:04 +0530

    navbar fixed alignment

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,14 +6,16 @@ import "./navbar.css"
 
 import UseAnimations from "react-useanimations";
 
+import useWindowDimensions from "../../helpers/WindowDimensions.js"
+
 
 const Navbar = () => {
    
    const [navbarVisibility, setNavbarVisibility] = useState(false);
+   const { width } = useWindowDimensions();
 
    function toggleVisibility(){
       setNavbarVisibility(!navbarVisibility)
-      console.log(navbarVisibility)
    }
 
    const customNavbar = {
@@ -25,131 +27,78 @@ const Navbar = () => {
       top: 0,
       left: 0,
       overflowX: "hidden",
+      paddingTop:"30vh",
       width: navbarVisibility ? "100vw" : "0",
    }
 
-   const customNavbarA = {
-      padding: "8px 8px 8px 32px",
+   const customNavbarEach = {
+      padding: "8px 0 8px 0",
       textDecoration: "none",
       color: "white",
       display: "block",
       transition: "0.3s",
+      fontSize: width>750?"10rem":"2rem",
+      textAlign: "center",
    }
 
    return (
       <div className="navbar">
 
-      <HLink to="/">
-      {
-      400>750?
-      <UseAnimations
-      animationKey="infinity"
-      size={50}
-      style={{ color: "white", cursor: "pointer", marginLeft: "7vw", marginTop: "-16vh", position: "absolute", zIndex: "5" }}
-      />:
-      <UseAnimations
-         animationKey="infinity"
-         size={30} 
-         style={{ color: "white", cursor: "pointer", marginLeft: "10vw", marginTop: "-11vh", position: "absolute", zIndex: "5" }}
-      />
-      }
-      </HLink>
-        
-        <div onClick={toggleVisibility}>
-        {
-      400>750?
-      <UseAnimations
-        animationKey="menu2"
-        size={40}
-        style={{ color: "white", cursor: "pointer", marginLeft: "89vw", marginTop: "-16vh", position: "absolute", zIndex: "5" }}
-      />:
-      <UseAnimations
-        animationKey="menu2"
-        size={30}
-        style={{ color: "white", cursor: "pointer", marginLeft: "85vw", marginTop: "-11vh", position: "absolute", zIndex: "5" }}
-      />
-      }
-       </div>
+          <HLink to="/">
+          {
+            width>750?<UseAnimations animationKey="infinity" size={50} style={{ color: "white", cursor: "pointer", marginLeft: "7vw", marginTop: "-16vh", position: "absolute", zIndex: "5" }}/>
+            :<UseAnimations animationKey="infinity" size={30} style={{ color: "white", cursor: "pointer", marginLeft: "10vw", marginTop: "-11vh", position: "absolute", zIndex: "5" }}/>
+          }
+          </HLink>
+            
+          <div onClick={toggleVisibility}>
+          {
+            width>750?<UseAnimations animationKey="menu2" size={40} style={{ color: "white", cursor: "pointer", marginLeft: "89vw", marginTop: "-16vh", position: "absolute", zIndex: "5" }}/>
+            :<UseAnimations animationKey="menu2" size={30} style={{ color: "white", cursor: "pointer", marginLeft: "85vw", marginTop: "-11vh", position: "absolute", zIndex: "5" }}/>
+          }
+          </div>
 
-       <div className="navbar" style={customNavbar}>
-         <HLink to="/" style={customNavbarA}>Home</HLink>
-         <a href="/projects" style={customNavbarA}>Projects</a>
-         <HLink to="/blog" style={customNavbarA} >Blog</HLink>
-         <a href="/contact" style={customNavbarA} >Contact</a>
-         <span style={{}}>
-         <a href="https://www.github.com/nikxtaco">
-       {
-      400>750?
-      <UseAnimations
-        animationKey="github"
-        size={50}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"38vw", paddingTop: "10vh" }}
-      />:
-      <UseAnimations
-        animationKey="github"
-        size={30}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "3vh" }}
-      />
-      }
-         </a>
-         <a href="https://instagram.com/nikxtaco">
-         {
-      400>750?
-      <UseAnimations
-        animationKey="instagram"
-        size={50}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}
-      />:
-      <UseAnimations
-        animationKey="instagram"
-        size={30}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "3vh" }}
-      />
-      }
-      </a>
-         <a href="https://www.linkedin.com/in/nikita-menon-b2248079">
-        {
-      400>750?
-      <UseAnimations
-        animationKey="linkedin"
-        size={50}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}
-      />:
-      <UseAnimations
-        animationKey="linkedin"
-        size={30}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "3vh" }}
-      />
-      }
-         </a>
-         <a href="https://twitter.com/nikxtaco">
-         {
-      400>750?
-      <UseAnimations
-        animationKey="twitter"
-        size={50}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}
-      />:
-      <UseAnimations
-        animationKey="twitter"
-        size={30}
-        style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "3vh" }}
-      />
-      }
-         </a>
-         </span>
-       </div>
-       
-       {/* <div className="look_up_bg">
-      <h6 className="look_up" onClick={() => {window.scrollTo(0, 0)}}>
-        <UseAnimations
-        animationKey="arrowUp"
-        size={20}
-        style={{ color: "black", cursor: "pointer", padding:"5px", margin:"0", float:"left", paddingBottom:"0"  }}
-      />
-      </h6>
-      </div> */}
+          <div className="navbar" style={customNavbar}>
+              <HLink to="/" style={customNavbarEach}>Home</HLink>
+              <a href="/projects" style={customNavbarEach}>Projects</a>
+              <HLink to="/blog" style={customNavbarEach} >Blog</HLink>
+                
+              <span>
+              <a href="https://www.github.com/nikxtaco">
+              {
+                width>750?<UseAnimations animationKey="github" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"32.5vw", paddingTop: "10vh" }}/>
+                :<UseAnimations animationKey="github" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"15vw", paddingTop: "5vh" }}/>
+              }</a>
 
+              <a href="https://instagram.com/nikxtaco">
+              {
+              width>750?<UseAnimations animationKey="instagram" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}/>
+              :<UseAnimations animationKey="instagram" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "5vh" }}/>
+              }</a>
+
+              <a href="https://www.linkedin.com/in/nikita-menon-b2248079">
+              {
+              width>750?<UseAnimations animationKey="linkedin" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}/>
+              :<UseAnimations animationKey="linkedin" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "5vh" }}/>
+              }</a>
+                
+              <a href="https://twitter.com/nikxtaco">
+              {
+              width>750?<UseAnimations animationKey="twitter" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}/>
+              :<UseAnimations animationKey="twitter" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "5vh" }}/>
+              }</a>
+              </span>
+          </div>
+          
+          {/* <div className="look_up_bg">
+          <h6 className="look_up" onClick={() => {window.scrollTo(0, 0)}}>
+            <UseAnimations
+            animationKey="arrowUp"
+            size={20}
+            style={{ color: "black", cursor: "pointer", padding:"5px", margin:"0", float:"left", paddingBottom:"0"  }}
+          />
+          </h6>
+          </div> */}
       </div>
    )
 }
