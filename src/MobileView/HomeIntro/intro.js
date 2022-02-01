8210498368fd16a9db81302cee34614a4470de21
nikxtaco <nikitamenon2510@gmail.com>
2022-02-01 06:11:06 +0530

    scroll fixes

--- src/MobileView/HomeIntro/intro.js
+++ src/MobileView/HomeIntro/intro.js
@@ -129,34 +129,6 @@ export default function Intro() {
           Until then, I fare thee well.<br/>
           </p>
 
-          {/* <span>
-              <a href="https://www.github.com/nikxtaco">
-              {
-                width>1440?<UseAnimations animationKey="github" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"32.5vw", paddingTop: "10vh" }}/>
-                :<UseAnimations animationKey="github" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"8vw", paddingTop: "5vh", paddingBottom:"5vh" }}/>
-              }</a>
-
-              <a href="https://instagram.com/nikxtaco">
-              {
-              width>1440?<UseAnimations animationKey="instagram" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}/>
-              :<UseAnimations animationKey="instagram" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "5vh" }}/>
-              }</a>
-
-              <a href="https://www.linkedin.com/in/nikita-menon-b2248079">
-              {
-              width>1440?<UseAnimations animationKey="linkedin" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}/>
-              :<UseAnimations animationKey="linkedin" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "5vh" }}/>
-              }</a>
-                
-              <a href="https://twitter.com/nikxtaco">
-              {
-              width>1440?<UseAnimations animationKey="twitter" size={"5vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"5vw", paddingTop: "10vh" }}/>
-              :<UseAnimations animationKey="twitter" size={"10vw"} style={{ color: "white", cursor: "pointer", padding:"0", margin:"0", float:"left" , paddingLeft:"10vw", paddingTop: "5vh" }}/>
-              }</a>
-              
-          </span> */}
-
-
 </div>
 
 
