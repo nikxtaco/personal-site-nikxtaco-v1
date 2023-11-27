63638c8a507635d8de8ee5d133f081b0230e8515
nikxtaco <nikitamenon2510@gmail.com>
2023-11-27 10:46:17 +0530

    Modified keyboard data

--- src/DesktopView/HomeProjects/projectStuff.js
+++ src/DesktopView/HomeProjects/projectStuff.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import "./projects.css"
+import UseAnimations from "react-useanimations";
 
 import FilterBubbleMockup from "../../img/projects/FilterBubbleMockup.png"
-// import alrtai from "../../img/projects/alrtai.jpg"
+import AlrtAIMockup from "../../img/projects/AlrtAIMockup.png"
 
 const ProjectStuff = () => {
-    const images = [FilterBubbleMockup]
+    const images = [
+      [FilterBubbleMockup, 'https://github.com/DSC-QGambit'], 
+      [AlrtAIMockup, 'https://github.com/nikxtaco/alrtai-landing']
+    ]
 
   return (
     <div className="iframe-container">
       {Object.entries(images).map(([title, path]) => (
         <div key={path}>
         <div>
-          <img
-            src={path}
+          <img className='iframe-projects-item'
+            src={path[0]}
             alt={title}
-            className='iframe-projects-item'
           />
+          <a href={path[1]} target="_blank" rel="noreferrer">
+                <UseAnimations animationKey="github" size={"3vmin"} style={{ float: 'left',  marginLeft: '-15.5vw', paddingTop: '17vw', color: 'white' }}/>
+            </a>
         </div>
         </div>
       ))}
